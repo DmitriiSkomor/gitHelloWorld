1b5cd24324eb518f3d3edb15d7eaa6ae9b25ac55
Dmitrii Skomorokhov <mr.skmr94@gmail.com>
2018-11-07 18:42:57 +0200

    addedTostring

--- src/Person.java
+++ src/Person.java
@@ -9,4 +9,11 @@ public class Person {
     public void setId(Long id) {
         this.id = id;
     }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                '}';
+    }
 }
